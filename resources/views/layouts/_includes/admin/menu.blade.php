2632718ec14a24d45a9d6b2bc9b3d40aa6abdec9
paulotumba <manuelpaulo@gec.inatel.br>
2023-10-12 22:19:28 +0100

    update

--- resources/views/layouts/_includes/admin/menu.blade.php
+++ resources/views/layouts/_includes/admin/menu.blade.php
@@ -22,9 +22,9 @@
                     <span class="ml-3 item-text">Dashboard</span><span class="sr-only">(current)</span>
                 </a>
             </li>
-            {{-- <p class="text-muted nav-heading mt-4 mb-1">
-                <span>Loja</span>
-            </p> --}}
+            <p class="text-muted nav-heading mt-4 mb-1">
+                <span>Categorias</span>
+            </p>
 
            <li class="nav-item dropdown">
                 {{-- <a href="#categoriaPost-collapse" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
@@ -60,6 +60,11 @@
             </li>
     </li>
             {{-- Logs --}}
+
+
+            <p class="text-muted nav-heading mt-4 mb-1">
+                <span>Titulos</span>
+            </p>
         </ul>
     </nav>
   </aside>
